b7d99fc9167325e0a9b37d198590779a31d6ee25
alexbarksdale <ale.barksdale@gmail.com>
2019-11-04 20:14:15 -0800

    project setup

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, '/public')));
+
+app.get('/', function(req, res) {
+    res.render('pages/home.ejs');
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, function() {
+    console.log(`Capstone running on port: http://localhost:${PORT}`);
+});
